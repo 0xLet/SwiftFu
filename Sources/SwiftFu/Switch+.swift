e2a830f8b4514925c80b4833e88eedbe976ebbd6
Zach Eriksen <zmeriksen@icloud.com>
2021-03-24 19:30:57 -0500

    Added file stubs

---
+++ Sources/SwiftFu/Switch+.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leif on 3/24/21.
+//
+
+import Foundation
---
+++ Tests/SwiftFuTests/SwitchTests.swift
@@ -0,0 +1,100 @@
+//
+//  SwitchTests.swift
+//
+//
+//  Created by Zach Eriksen on 7/7/20.
+//
+
+import XCTest
+@testable import SwiftFu
+
+final class SwitchTests: XCTestCase {
+    
+    func test_switchmap() {
+        let value = "qwerty"
+        
+        let switchedValue = switchmap(
+                                      value: value,
+                                      cases: [
+                                              0: false,
+                                              3.14: false,
+                                              "qwerty": true
+                                              ],
+                                      default: nil
+                                      )
+        
+        XCTAssertEqual(switchedValue, true)
+    }
+    
+    func test_switcheach() {
+        enum Example {
+        case a, b, c, d
+        }
+        
+        let value: Example = .c
+        
+        switcheach(
+                   value: value,
+                   cases: [
+                           .a: {
+            XCTFail()
+        },
+                           .b: {
+            XCTFail()
+        }
+                           ],
+                   default:  {
+            XCTAssert(true)
+        }
+                   )
+    }
+    
+    func test_switchmapArray() {
+        let list = [1, 2, 3]
+        
+        let switchedList = list.switchmap(cases: [
+                                                  0: "zero",
+                                                  1: "one",
+                                                  2: "two",
+                                                  3: "three"
+                                                  ], default: "Nil")
+        
+        XCTAssertEqual(switchedList, ["one", "two", "three"])
+    }
+    
+    func test_switcheachArray() {
+        let list = [1, 2, 3]
+        var count = 0
+        
+        list.switcheach(cases: [
+                                0: { count += 10 },
+                                1: { count += 20 },
+                                2: { count += 30 },
+                                3: { count += 40 }
+                                ], default: { count = -1 })
+        
+        XCTAssertEqual(count, 90)
+    }
+    
+    func test_switchmapString() {
+        let list = "123"
+        
+        let switchedList = list.switchmap(cases: [
+                                                  "0": "zero",
+                                                  "1": "one",
+                                                  "2": "two",
+                                                  "3": "three"
+                                                  ], default: "Nil")
+        
+        XCTAssertEqual(switchedList, ["one", "two", "three"])
+    }
+    
+    
+    static var allTests = [
+                           ("test_switchin", test_switchmap),
+                           ("test_switchinClosure", test_switcheach),
+                           ("test_switchinArray", test_switchmapArray),
+                           ("test_switcheachArray", test_switcheachArray),
+                           ("test_switchinString", test_switchmapString)
+                           ]
+}
