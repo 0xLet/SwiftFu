00b9e572ccd7a7175e2ef4047228d8d9265d1221
Zach Eriksen <zmeriksen@icloud.com>
2021-05-11 20:46:02 -0500

    Support linux

--- Sources/SwiftFu/Foundation+SwiftFu.swift
+++ Sources/SwiftFu/Foundation+SwiftFu.swift
@@ -2,7 +2,10 @@ import Foundation
 
 extension Data: Fuable { }
 extension Date: Fuable { }
+
+#if os(Linux)
+#else
 extension URL: Fuable { }
 extension URLRequest: Fuable { }
-
 extension NSObject: FuableClass { }
+#endif
